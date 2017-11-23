3e4722a0d07c8f5275584d50c4bc44e557dcb77e
puolimatkankrouvi <cde_1414@yahoo.com>
2017-11-23 19:07:30 +0200

    added GetDucks file

---
+++ src/GetDucks.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import React, {Component} from 'react';
+var moment = require('moment');
+require('moment/locale/fi');
+
+
+
+const backend_url = "http://localhost:8081/sightings";
+
+const date_format = 'D.MM.YYYY, hh:mm:ss';
+
+
+
+class GetDucks extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {sightings: [], all_species: [] };
+  }
+
+  componentDidMount(){
+    
+    //Fetches all current sightings from server
+    axios.get( backend_url )
+      .then( response => {
+        const sightings = response.data;
+        this.setState({sightings});
+      }
+    );
+
+  }
+
+  render (){
+    return(
+      [
+        //Mapping duck-sightings to rows of table
+        // and each parameter of a sighting to a column
+        this.state.sightings.map( sighting =>
+          (
+            <tr>
+              <th>
+                {sighting.count}
+              </th>
+              <th>
+                {sighting.species}
+              </th>
+              <th>
+                {sighting.description}
+              </th>
+              <th>
+                {moment(sighting.dateTime).format( date_format )}
+              </th>
+            </tr>
+          )
+        )
+      ]
+    )
+  }
+
+}
+
+export default GetDucks;
