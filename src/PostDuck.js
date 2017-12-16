0236dc3cb56b5586cc95da722e052e04e0ebf8b4
puolimatkankrouvi <cde_1414@yahoo.com>
2017-12-16 12:53:57 +0200

    version before refactor

--- src/PostDuck.js
+++ src/PostDuck.js
@@ -6,42 +6,6 @@ import axios from 'axios';
 
 const backend_url = 'http://localhost:8081/sightings';
 
-
-
-/*class PostDuck extends Component{
-
-  constructor(props){
-    super(props);
-    this.state = { count: 4, species: 'mallard', description: 'asjdlajskdlj', sightingDate: Date()};
-  }
-
-  componentDidMount(){
-    //Sending a new sighting through POST
-    axios.post(
-      backend_url,
-      //JSON
-      { 
-      	'count': this.state.count,
-      	'species': this.state.species,
-      	'description': this.state.description,
-      	'dateTime': this.state.sightingDate
-      }
-    )
-    .then( (response) => {
-    	
-    });
-  }
-
-  render(){
-  	return null;
-  }
-}
-
-export default PostDuck;*/
-
-
-//The above React class could be just a simple javascript function:
-
 export function PostDuck( count = 4, species = 'mallard', description = 'asjdlajskdlj', sightingDate= Date ){
   //Sending a new sighting through POST
   axios.post(
